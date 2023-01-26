6ef3bce73cf987ea2d00b4bc27c96243c9e94a2d
pedr <pedrlz.frn@gmail.com>
2023-01-26 10:08:51 -0300

    test: unit test for AlertWarning component

---
+++ src/components/AlertWarning.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import AlertWarning from './AlertWarning'
+
+describe('components/AlertWarning', () => {
+
+  test('should show message if isVisible is true', () => {
+    // ARRANGE
+    const message = 'mock-message'
+    const isVisible = true
+    render(<AlertWarning isVisible={isVisible} message={message} />)
+
+    expect(screen.queryByText(message)).toBeVisible();
+
+  })
+
+  test('should NOT show message if isVisible is false', () => {
+    // ARRANGE
+    const message = 'mock-message'
+    const isVisible = false
+    render(<AlertWarning isVisible={isVisible} message={message} />)
+
+    expect(screen.queryByText(message)).toBeNull();
+  })
+})
